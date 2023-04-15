19007cd35e7296c455332e0d7e34fa6734a35502
Linno87 <99483714+Linno87@users.noreply.github.com>
2023-04-15 13:27:08 +0000

    commit

--- TP1/app.js
+++ TP1/app.js
@@ -3,20 +3,45 @@ console.clear()
 const process = require("process")
 const operaciones = require("./calculadora.js")
 
-let n1 = +process.argv[2]
-let n2 = process.argv[3]
-let n3 = +process.argv[4]
 
-if( isNaN(n1)|| isNaN(n3) ){
-    console.log("Por favor, ingrese caracteres numericos")}
+let numeroUno = +process.argv[2]
+let operacionARealizar = process.argv[3]
+let numeroDos = +process.argv[4]
 
-const operacion = ["suma", "resta", "multiplicacion", "division", "+", "-", "*", "/"];
-if(!operacion.includes(n2)){
-    console.log("Operacion no valida")
+const resultado = function (n1, n2, n3){
+
+    if( isNaN(n1)|| isNaN(n3) ){
+     return "Por favor, ingrese caracteres numericos";
+    }
+
+    const operacion = ["suma", "resta", "multiplicacion", "division", "modulo", "+", "-", "*", "/", "%"];
+    if(!operacion.includes(n2)){
+        if( isNaN(n2) ){
+            return "Operacion no valida";
+        } else {
+            return "El formato debe ser ´A Operacion B´ ";
+        }
+    }
+    if(n2 === "suma" || n2==="+"){
+        return operaciones.suma(n1, n3);
+    }
+    if(n2==="resta" || n2==="-"){
+        return operaciones.resta(n1, n3);
+    }
+    if(n2==="multiplicacion" || n2==="*"){
+        return operaciones.multiplicacion(n1, n3);
+    }
+    if(n2==="division" || n2==="/"){
+        return operaciones.division(n1, n3);
+    }
+    
+    if(n2==="modulo" || n2==="%"){
+        return operaciones.modulo(n1, n3);
+    }
+
 }
-
-
-console.log(n1, n2, n3)
+const result = resultado(numeroUno,operacionARealizar, numeroDos);
+console.log(result)
 
 
 
